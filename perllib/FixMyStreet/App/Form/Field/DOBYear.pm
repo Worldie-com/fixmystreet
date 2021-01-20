4d502265dcaf173229ada8dda1bd8c9601e1618b
Struan Donald <struan@exo.org.uk>
2021-01-20 13:53:46 +0000

    fixup! make date fields work

--- perllib/FixMyStreet/App/Form/Field/DOBYear.pm
+++ perllib/FixMyStreet/App/Form/Field/DOBYear.pm
@@ -2,6 +2,8 @@ package FixMyStreet::App::Form::Field::DOBYear;
 use Moose;
 extends 'HTML::FormHandler::Field::IntRange';
 
+has min_age => ( is => 'ro', default => 16 );
+
 has '+range_start' => (
     default => sub {
         return 1900;
@@ -9,7 +11,8 @@ has '+range_start' => (
 );
 has '+range_end' => (
     default => sub {
-        my $year = (localtime)[5] + 1900;
+        my $self = shift;
+        my $year = (localtime)[5] - $self->min_age + 1900;
         return $year;
     }
 );
